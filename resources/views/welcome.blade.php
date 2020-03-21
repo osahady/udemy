2cebcf74dc20ffaa72dad129b0f9ba0f9d36e27e
abusarya <abukarmo194@gmail.com>
2020-03-21 17:38:57 +0300

    Text Center

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
+                <div class="title m-b-md text-center">
                     Git Flow Greatings New
                 </div>
 
