d9715a70db9cc962fdcc4cd223d1a9e247e4c9ed
abusarya <abukarmo194@gmail.com>
2020-03-21 17:31:11 +0300

    New

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Git Flow Greatings
+                    Git Flow Greatings New
                 </div>
 
                 <div class="links">
