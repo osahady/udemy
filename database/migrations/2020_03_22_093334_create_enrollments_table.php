a46fc6e882e9e726aed96066465ffce8e9127910
osahady <osahady@gmail.com>
2020-03-22 17:26:30 +0300

    adding unique to enrollemnt and role_user tables

--- database/migrations/2020_03_22_093334_create_enrollments_table.php
+++ database/migrations/2020_03_22_093334_create_enrollments_table.php
@@ -15,13 +15,14 @@ class CreateEnrollmentsTable extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->index();
             $table->unsignedBigInteger('course_id')->index();
+            $table->unsignedBigInteger('user_id')->index();
             $table->string('reason')->nullable();
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->unique(['course_id', 'user_id']);
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('CASCADE');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
         });
     }
 
--- database/migrations/2020_03_22_141503_role_user.php
+++ database/migrations/2020_03_22_141503_role_user.php
@@ -15,12 +15,13 @@ class RoleUser extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
         
-            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['role_id', 'user_id']);
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
     }
