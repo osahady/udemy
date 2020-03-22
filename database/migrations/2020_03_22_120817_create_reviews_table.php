ed228af8b128566478a947f99d380e3cbdb22145
abusarya <abukarmo194@gmail.com>
2020-03-22 15:13:24 +0300

    Review

---
+++ database/migrations/2020_03_22_120817_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->unsignedBigInteger('enrollment_id');
+            $table->integer('stars')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+
+            $table->foreign('enrollment_id')->references('id')->on('enrollments')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
