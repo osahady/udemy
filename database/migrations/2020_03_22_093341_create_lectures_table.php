0568a887c1563280aff091fd695d8a7742310080
osahady <osahady@gmail.com>
2020-03-22 12:56:24 +0300

    lectures table created

---
+++ database/migrations/2020_03_22_093341_create_lectures_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('section_id');
+            $table->string('title');
+            $table->time('duration');
+            $table->timestamps();
+
+            $table->foreign('section_id')->references('id')->on('sections')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures');
+    }
+}
