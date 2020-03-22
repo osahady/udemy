005e13720ee114c6ad5eda258598c216c6dd87a4
osahady <osahady@gmail.com>
2020-03-22 11:08:35 +0300

    resume is nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->text('resume');
+            $table->text('resume')->nullable();
             $table->string('locale', 3)->default('en');
             $table->timestamps();
         });
