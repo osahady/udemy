36919d5e7118e47517b64fabbfa2bb2c2c045ba3
abusarya <abukarmo194@gmail.com>
2020-03-26 16:16:53 +0300

    Category Factory-Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,22 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'resume' => $faker->paragraphs(),
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->state(User::class, 'osama', function (Faker $faker) {
+    return [
+        'name' => 'Osama',
+        'email' => 'osahady@gmail.com',
+    ];
+});
+
+
+$factory->state(User::class, 'obada', function (Faker $faker) {
+    return [
+        'name' => 'Obada',
+        'email' => 'o@o.com',
     ];
 });
