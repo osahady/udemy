ffc6eb6084c640740f97b08db13841ea81ca58b4
osahady <osahady@gmail.com>
2020-03-26 16:21:40 +0300

    DatabaseSeeder run function conflict has been removed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         $this->call(RolesTableSeeder::class);
-=======
         $this->call(CategoriesTableSeeder::class);
->>>>>>> 36919d5e7118e47517b64fabbfa2bb2c2c045ba3
     }
 }
