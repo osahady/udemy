649c3aa44390c764acff73ed67a37c04cc077120
abusarya <abukarmo194@gmail.com>
2020-03-28 14:59:12 +0300

    Pull FbQuestionsTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,12 +19,8 @@ class DatabaseSeeder extends Seeder
             SectionsTableSeeder::class,
             EnrollmentsTableSeeder::class,
             LecturesTableSeeder::class,
-<<<<<<< HEAD
             RoleUserTableSeeder::class,
-=======
-            FbQestionsTableSeeder::class
- 
->>>>>>> 2574848642ab4741d8a5f7b23846139bbb8e1ed3
+            FbQestionsTableSeeder::class,
         ]);
     }
 }
