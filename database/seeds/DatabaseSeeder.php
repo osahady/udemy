2574848642ab4741d8a5f7b23846139bbb8e1ed3
osahady <osahady@gmail.com>
2020-03-28 14:16:18 +0300

    questions and answers seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,24 +12,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-<<<<<<< HEAD
-            // RolesTableSeeder::class,
-            // CategoriesTableSeeder::class,
-            // UsersTableSeeder::class,
-            // CoursesTableSeeder::class,
-            // SectionsTableSeeder::class,
-            // EnrollmentsTableSeeder::class,
-            // LecturesTableSeeder::class,
-            FbQestionsTableSeeder::class
-=======
             RolesTableSeeder::class,
             CategoriesTableSeeder::class,
             UsersTableSeeder::class,
             CoursesTableSeeder::class,
             SectionsTableSeeder::class,
             EnrollmentsTableSeeder::class,
-            LecturesTableSeeder::class
->>>>>>> 3a1d7c597d76141e9bb02256ad0afc69b80f5bd0
+            LecturesTableSeeder::class,
+            FbQestionsTableSeeder::class
+ 
         ]);
     }
 }
