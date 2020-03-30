96fe83288125935b418ba72b04c70a9b8f5d7303
osahady <osahady@gmail.com>
2020-03-30 18:39:58 +0300

    fixing return $this->belongsToMany(AppRole)->withTimestamps();

--- app/User.php
+++ app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany('App\Role');
+        return $this->belongsToMany('App\Role')->withTimestamps();
     }
 
     public function image()
